3aa7fa649784845fd9b64083c8e160848e88792a
Van Sy <vansy151101@gmail.com>
2024-06-20 16:12:21 +0700

    feat: Add configuration to settings

--- pdf_parser/src/config/settings.py
+++ pdf_parser/src/config/settings.py
@@ -6,15 +6,13 @@ class Settings(BaseSettings):
     api_key: Optional[str]
     ai_model_name: Optional[str]
     temperature: Optional[float]
+    database_url: Optional[str]
+    db_name: Optional[str]
     
-    # model_config = SettingsConfigDict(env_file="", env_file_encoding='utf-8', extra="ignore")
     class Config:
         env_file = ".env"
         env_file_encoding = 'utf-8'
         extra = "ignore"
     
 def get_settings(env_file_path) -> Settings:
-    # return Settings.model_config(env_file = env_file_path)
     return Settings(_env_file=env_file_path)
-
-# settings = get_settings()
